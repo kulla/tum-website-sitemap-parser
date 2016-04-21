034d9ea22146658eb0fe26d1cfb29555becb33ab
Stephan Kulla <git.mail@kulla.me>
2016-04-21 17:01:32 +0200

    merge api.py and parse_sitemap.py

--- parse_sitemap.py
+++ parse_sitemap.py
@@ -1,6 +1,109 @@
+import requests
 import sys
 
-from api import TUMSitemap
+from bs4 import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
+
+class Node(object):
+
+    def __init__(self, name, url):
+        self.name = str(name)
+        self.url = url
+        self.children = []
+
+    def append_nodes_from_html(self, ul, base_url):
+        for li in ul.find_all("li", recursive=False):
+            a = li.find("a", recursive=False)
+
+            if a:
+                if a["href"].startswith("/"):
+                    url = base_url.rstrip("/") + a["href"]
+                else:
+                    url = a["href"]
+
+                node = Node(a.string, url)
+            else:
+                strong = li.find("strong", recursive=False)
+                node = Node(strong.string, None)
+
+            subul = li.find("ul", recursive=False)
+
+            if subul:
+                node.append_nodes_from_html(subul, base_url)
+
+            self.children.append(node)
+
+    @property
+    def urls(self):
+        result = set([ self.url ])
+
+        for child in self.children:
+            result.update(child.urls)
+
+        return result
+
+    def merge(self, other):
+        assert self.name == other.name
+
+        self.url = self.url or other.url
+
+        assert other.url == None or (self.url == other.url)
+
+        self.children = self.children or other.children
+
+        assert len(other.children) == 0 or len(self.children) == len(other.children)
+
+        for schild, ochild in zip(self.children, other.children):
+            schild.merge(ochild)
+
+    def __str__(self, level=0):
+        result = " "*level + self.name
+
+        for child in self.children:
+            result += "\n" + child.__str__(level+1)
+
+        return result
+
+class TUMSitemap(object):
+
+    def __init__(self, root_name, base_url):
+        self.root = Node(root_name, base_url)
+        self.visited = set()
+
+    def fetch_sitemap(self):
+        while True:
+            urls = self.root.urls - self.visited
+
+            if len(urls) > 0:
+                next_url = urls.pop()
+
+                self.visited.add(next_url)
+
+                if next_url.startswith(self.root.url):
+                    self.root.merge(self.parse_sitemap_from_site(next_url))
+            else:
+                break
+
+        return self.root
+
+    def parse_sitemap_from_site(self, url):
+        html = BeautifulSoup(requests.get(url).text)
+
+        sitemenu = html.find("nav", class_="sitemenu").find("ul")
+
+        root = Node(self.root.name, self.root.url)
+        root.append_nodes_from_html(sitemenu, self.root.url)
+
+        self.visited.add(url)
+
+        return root
+
+    def to_html(self):
+        env = Environment(loader = FileSystemLoader("."))
+
+        template = env.get_template("outline.template.html")
+
+        return template.render(sitemap = self.fetch_sitemap())
 
 if __name__ == "__main__":
     assert len(sys.argv) >= 4
