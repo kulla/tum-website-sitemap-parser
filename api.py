5faf59b46462552e3c852bae181c0e293c2cc49f
Stephan Kulla <git.mail@kulla.me>
2016-04-21 15:11:55 +0200

    implementing the fetching of the navigation

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 class Node(object):
 
-    def __init__(self, name, link):
+    def __init__(self, name, url):
         self.name = str(name)
-        self.link = link
+        self.url = url
         self.children = []
 
     def append_nodes_from_html(self, ul, base_url):
@@ -10,7 +10,12 @@ class Node(object):
             a = li.find("a", recursive=False)
 
             if a:
-                node = Node(a.string, base_url.rstrip("/") + a["href"])
+                if a["href"].startswith("/"):
+                    url = base_url.rstrip("/") + a["href"]
+                else:
+                    url = a["href"]
+
+                node = Node(a.string, url)
             else:
                 strong = li.find("strong", recursive=False)
                 node = Node(strong.string, None)
@@ -21,6 +26,29 @@ class Node(object):
                 node.append_nodes_from_html(subul, base_url)
 
             self.children.append(node)
+
+    @property
+    def urls(self):
+        result = set([ self.url ])
+
+        for child in self.children:
+            result.update(child.urls)
+
+        return result
+
+    def merge(self, other):
+        assert self.name == other.name
+
+        self.url = self.url or other.url
+
+        assert other.url == None or (self.url == other.url)
+
+        self.children = self.children or other.children
+
+        assert len(other.children) == 0 or len(self.children) == len(other.children)
+
+        for schild, ochild in zip(self.children, other.children):
+            schild.merge(ochild)
 
     def __str__(self, level=0):
         result = " "*level + self.name
--- fetch_navigation.py
+++ fetch_navigation.py
@@ -3,15 +3,43 @@ import requests
 from api import Node
 from bs4 import BeautifulSoup
 
-def get_navigation_from_site(url, title):
-    html = BeautifulSoup(requests.get(url).text)
+class SitemapFetcher(object):
 
-    sitemenu = html.find("nav", class_="sitemenu").find("ul")
+    def __init__(self, root_name, base_url):
+        self.root = Node(root_name, base_url)
+        self.visited = set()
 
-    root = Node(title, url)
-    root.append_nodes_from_html(sitemenu, url)
+    def fetch_sitemap(self):
+        while True:
+            urls = self.root.urls - self.visited
 
-    return root
+            if len(urls) > 0:
+                next_url = urls.pop()
+
+                self.visited.add(next_url)
+
+                print(next_url)
+
+                if next_url.startswith(self.root.url):
+                    self.root.merge(self.parse_sitemap_from_site(next_url))
+            else:
+                break
+
+        return self.root
+
+    def parse_sitemap_from_site(self, url):
+        html = BeautifulSoup(requests.get(url).text)
+
+        sitemenu = html.find("nav", class_="sitemenu").find("ul")
+
+        root = Node(self.root.name, self.root.url)
+        root.append_nodes_from_html(sitemenu, self.root.url)
+
+        self.visited.add(url)
+
+        return root
 
 if __name__ == "__main__":
-    print(get_navigation_from_site("http://www.ma.edu.tum.de/mitarbeiter", "Fakultätsseite"))
+    fetcher = SitemapFetcher("Fakultätsseite", "http://www.ma.edu.tum.de/")
+
+    print(fetcher.fetch_sitemap())
